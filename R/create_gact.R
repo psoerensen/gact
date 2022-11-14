3927297553bb8bdd8aa4fd29ac78556cf7532083
Peter Sørensen <au223366@uni.au.dk>
2022-11-14 09:14:36 +0100

    Update create_gact.R

--- R/create_gact.R
+++ R/create_gact.R
@@ -285,6 +285,93 @@ gact <- function(GAlist=NULL, version="t2dm-gact-0.0.1", task="download",
 
 #' @export
 #'
+getStatDB <- function(GAlist=NULL, feature=NULL, featureID=NULL,file=NULL,
+                    studyID=NULL, trait=NULL, threshold=NULL,
+                    format="data.frame") {
+ features <- c("Markers","Genes","Proteins","GO","Pathways",
+               "ProteinComplexes","ChemicalComplexes","Chromosomes")
+ if(!feature%in%features) stop(paste("feature:",feature,"not in database"))
+ header <- c("Marker ID","Gene ID","Protein ID","GO ID","Pathway ID",
+             "Protein ID","Chemical ID", "Chromosome ID")
+ names(header) <- features
+
+ gseafile <- paste0(GAlist$dirs["gsea"],"ct_gsea",feature,"_gdtdb.rds")
+ res <- readRDS(gseafile)
+
+ colnames(res$stat) <- gsub("z_","",colnames(res$stat))
+ colnames(res$p) <- gsub("z_","",colnames(res$p))
+ res$p[res$stat==0] <- 1
+ if(!is.null(threshold)) {
+  cls <- grep(paste0("_",as.character(threshold)),colnames(res$stat))
+  res$stat <- res$stat[,cls]
+  res$p <- res$p[,cls]
+  colnames(res$stat) <- gsub(paste0("_",as.character(threshold)),"",colnames(res$stat))
+  colnames(res$p) <- gsub(paste0("_",as.character(threshold)),"",colnames(res$p))
+ }
+ if(format=="data.frame") {
+  res <- as.data.frame(res)
+  if(feature=="Genes") {
+   res <- cbind(rownames(res),GAlist$gsets[["ensg2sym"]][rownames(res)], res)
+   colnames(res)[1:2] <- c("Ensembl Gene ID","Symbol")
+
+  }
+ }
+ return(res)
+}
+
+
+#' @export
+#'
+writeStatDB <- function(GAlist=NULL, feature=NULL, featureID=NULL,
+                      studyID=NULL, trait=NULL, threshold=NULL,
+                      format="data.frame", file.csv=NULL, hyperlink=TRUE) {
+ res <- getStatDB(GAlist=GAlist, feature=feature, featureID=featureID,
+                 studyID=studyID, trait=trait, threshold=threshold,
+                 format=format)
+ features <- c("Markers","Genes","Proteins","GO","Pathways",
+               "ProteinComplexes","ChemicalComplexes","Chromosomes")
+ if(!feature%in%features) stop(paste("feature:",feature,"not in database"))
+ header <- c("Marker ID","Gene ID","Protein ID","GO ID","Pathway ID",
+             "Protein ID","Chemical ID", "Chromosome ID")
+ names(header) <- features
+ if(feature=="Genes") {
+  if(hyperlink) {
+   res <- cbind(rownames(res), res)
+   res2hyperlink_ensembl <- paste0("http://www.ensembl.org/Homo_sapiens/Gene/Summary?g=",res[,1])
+   res2hyperlink_opentarget <- paste0("https://platform.opentargets.org/target/",res[,1])
+   colnames(res)[1:3] <- c("Ensembl Gene ID","Open Target","Symbol")
+
+   res2hyperlink_ensembl <- paste0("=Hyperlink(",'"',res2hyperlink_ensembl,'"',";",'"',res[,1],'"',")")
+   res2hyperlink_opentarget <- paste0("=Hyperlink(",'"',res2hyperlink_opentarget,'"',";",'"',res[,1],'"',")")
+   res[,1] <- res2hyperlink_ensembl
+   res[,2] <- res2hyperlink_opentarget
+  }
+ }
+ res2html <- c("https://www.ncbi.nlm.nih.gov/snp/",
+               "http://www.ensembl.org/Homo_sapiens/Gene/Summary?g=",
+               "http://www.ensembl.org/Homo_sapiens/Protein/Summary?p=",
+               "https://www.ebi.ac.uk/QuickGO/term/",
+               "https://reactome.org/content/detail/",
+               "https://string-db.org/network/9606.",
+               "https://pubchem.ncbi.nlm.nih.gov/compound/")
+
+ names(res2html) <- c("Marker","Genes","Proteins","GO","Pathways",
+                      "ProteinComplexes","ChemicalComplexes")
+
+ if(!feature=="Genes") {
+  res <- cbind(rownames(res), res)
+  res2hyperlink <- paste0(res2html[feature],res[,1])
+  if(feature=="ChemicalComplexes") res2hyperlink <- paste0(res2html[feature],substring(res[,1],5,nchar(as.character(res[,1]))))
+  res2hyperlink <- paste0("=Hyperlink(",'"',res2hyperlink,'"',";",'"',res[,1],'"',")")
+  if(hyperlink) res[,1] <- res2hyperlink
+  colnames(res)[1] <- header[feature]
+ }
+
+ write.csv2(res,file=file.csv,row.names=FALSE)
+}
+
+#' @export
+#'
 getStat <- function(GAlist=NULL, feature=NULL, featureID=NULL,
                     studyID=NULL, trait="t2d", threshold=1,
                     format="data.frame", hyperlink=FALSE, cls=NULL) {
