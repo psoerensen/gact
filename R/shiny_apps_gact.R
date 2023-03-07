2e739aa2f45846a834f5cc92746693ca4f5175fa
Peter Sørensen <au223366@uni.au.dk>
2023-03-07 16:09:08 +0100

    Create shiny_apps_gact.R

---
+++ R/shiny_apps_gact.R
@@ -0,0 +1,70 @@
+#' @export
+#'
+
+shinyAppsDB <- function(GAlist=NULL, what="DrugDBTables") {
+
+ if(what=="DrugDBTables") {
+  require(shiny)
+  require(data.table)
+  require(DT)
+
+  shinyApp(
+   ui <- fluidPage(
+    sidebarLayout(
+     sidebarPanel(
+      selectInput("what", "Select a file to load:",
+                  choices = c("Drug Labels PharmGKB",
+                              "Drug Labels Genes PharmGKB",
+                              "Variants PharmGKB",
+                              "Automated Annotations PharmGKB",
+                              "Drugs DGI",
+                              "Genes DGI",
+                              "Categories DGI",
+                              "Interactions DGI",
+                              "Direct Associations OpenTarget",
+                              "Direct Associations By Data Type OpenTarget",
+                              "Direct Associations By Data Source OpenTarget"))
+     ),
+     mainPanel(
+      DTOutput("data")
+     )
+    )
+   )
+   ,
+   server <- function(input, output) {
+
+    observeEvent(input$what, {
+     if (input$what == "Drug Labels PharmGKB") {
+      drugdb_file <- file.path(GAlist$dirs["pharmgkb"], "drugLabels.tsv")
+     } else if (input$what == "Drug Labels Genes PharmGKB") {
+      drugdb_file <- file.path(GAlist$dirs["pharmgkb"], "drugLabels.byGene.tsv")
+     } else if (input$what == "Variants PharmGKB") {
+      drugdb_file <- file.path(GAlist$dirs["pharmgkb"], "clinicalVariants.tsv")
+     } else if (input$what == "Automated Annotations PharmGKB") {
+      drugdb_file <- file.path(GAlist$dirs["pharmgkb"], "automated_annotations.tsv")
+     } else if (input$what == "Drugs DGI") {
+      drugdb_file <- file.path(GAlist$dirs["pharmgkb"], "drugs.tsv")
+     } else if (input$what == "Genes DGI") {
+      drugdb_file <- file.path(GAlist$dirs["pharmgkb"], "genes.tsv")
+     } else if (input$what == "Categories DGI") {
+      drugdb_file <- file.path(GAlist$dirs["pharmgkb"], "categories.tsv")
+     } else if (input$what == "Interactions DGI") {
+      drugdb_file <- file.path(GAlist$dirs["pharmgkb"], "interactions.tsv")
+     } else if (input$what == "Direct Associations OpenTarget") {
+      drugdb_file <- file.path(GAlist$dirs["opentargets"], "associationByOverallDirect.tsv")
+     } else if (input$what == "Direct Associations By Data Type OpenTarget") {
+      drugdb_file <- file.path(GAlist$dirs["opentargets"], "associationByDatatypeDirect.tsv")
+     } else if (input$what == "Direct Associations By Data Source OpenTarget") {
+      drugdb_file <- file.path(GAlist$dirs["opentargets"], "associationByDatasourceDirect.tsv")
+     }
+     df <- fread(drugdb_file, quote = "", data.table = FALSE)
+     output$data <- renderDT(df,
+                             options = list(pageLength = 50, dom = 'Bfrtip',
+                                            buttons = c('copy', 'csv')),
+                             extensions = "Buttons")
+    })
+   }
+
+  )
+ }
+}
