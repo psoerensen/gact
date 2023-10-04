573e257054851a72e2ab4e41fa044f6ca57d7f17
Peter Sørensen <au223366@uni.au.dk>
2023-10-04 20:06:42 +0200

    Update create_gact.R

--- R/create_gact.R
+++ R/create_gact.R
@@ -887,12 +887,11 @@ createSetsDB <- function(GAlist = NULL, what=NULL,
  setsfile <- file.path(GAlist$dirs["gsets"], "ensg2rsids.rds")
  saveRDS(ensg2rsids, file = setsfile)
 
- markerSets <- readRDS(file=file.path(GAlist$dirs["gsets"],"ensg2rsids.rds"))
- markerSets <- mapSetsDB(sets=markerSets, featureID=GAlist$markers$rsids, index=TRUE)
- chr <- sapply(markerSets, function(x) {unique(GAlist$markers$chr[x])})
- start <- sapply(markerSets, function(x) {min(GAlist$markers$pos[x])})
- stop <- sapply(markerSets, function(x) {max(GAlist$markers$pos[x])})
- df <- data.frame(geneID=names(markerSets),chr,start,stop)
+ sets <- mapSetsDB(sets=ensg2rsids, featureID=GAlist$markers$rsids, index=TRUE)
+ chr <- sapply(sets, function(x) {unique(GAlist$markers$chr[x])})
+ start <- sapply(sets, function(x) {min(GAlist$markers$pos[x])})
+ stop <- sapply(sets, function(x) {max(GAlist$markers$pos[x])})
+ df <- data.frame(EnsemblID=names(sets),chr=chr,start=start,stop=stop)
  saveRDS(df, file = file.path(GAlist$dirs["gsets"], "genesplus_annotation.rds"))
 
 
